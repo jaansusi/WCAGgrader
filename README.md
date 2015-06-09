92db67c0614aea85036e87ec99fb8862a4eef271
Jaan Susi <jaan.susi@gmail.com>
2015-06-09 10:52:14 +0300

    Trying to get rid of the nosuchmethoderror

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # WCAGgrader
 
-Currently only works when executing from the root folder since access\_lint is not linked dynamicis not linked dynamically
+Currently only works when executing from the root folder since access_lint is not linked dynamicis not linked dynamically
--- build-plugin.sh
+++ build-plugin.sh
@@ -3,6 +3,7 @@
 #cd /home/jaan/Dropbox/Ülikool/Baka/java/nutchwax-0.13/
 echo "Building nutchWAX:"
 ant
-#rm -dr plugins/parse-htmlraw/*
-cp build/plugins/parse-htmlraw/* plugins/parse-htmlraw/
+mkdir plugins
+rm -dr plugins/*
+cp -r build/plugins/* plugins/
 echo "Copying done!"
