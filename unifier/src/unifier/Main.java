bfe0c58b321928dfeb01693ca744f025df2eaad2
Jaan Susi <jaan.susi@gmail.com>
2015-06-25 20:58:58 +0300

    Start on unifier

---
+++ unifier/src/unifier/Main.java
@@ -0,0 +1,36 @@
+package unifier;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String host = "jdbc:mysql://localhost:3306/mydb";
+		String user = "root";
+		String pass = "toor";
+		
+		
+		
+		//Try creating the connection
+		//>----------------------------------------------->
+		try {
+			Connection con = DriverManager.getConnection(host, user, pass);
+			Statement query = con.createStatement();
+			String select = "SELECT * FROM `Single`";
+			query.execute(select);
+			//String sql = "INSERT INTO `Single` (" + fields + ") VALUES (" + values + ");";
+			//query.execute(sql);
+			
+		} catch (SQLException e) {
+			System.out.println(e);
+		    System.out.println("SQLException: " + e.getMessage());
+		    System.out.println("SQLState: " + e.getSQLState());
+		    System.out.println("VendorError: " + e.getErrorCode());
+		}
+	}
+
+}
