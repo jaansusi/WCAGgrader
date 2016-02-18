e2b1776cf8a5bfed1a32a4f62e8af3de43e97a5e
Jaan Susi <jaan.susi@gmail.com>
2016-02-18 19:45:28 +0200

    Started work on regex to parse codesniffer output

--- grader/src/plugin/parse-htmlraw/src/java/ee/ut/cs/Uploader.java
+++ grader/src/plugin/parse-htmlraw/src/java/ee/ut/cs/Uploader.java
@@ -7,6 +7,9 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.text.SimpleDateFormat;
 import java.lang.String;
+import java.lang.Error;
+import java.util.regex.*;
+
 public class Uploader {
 
 	private static String host = "jdbc:mysql://localhost:3306/mydb";
@@ -76,28 +79,47 @@ public class Uploader {
 		System.out.println("Element count in answer set: " + array.size());
 		System.out.println("Errors : " + i);
 		System.out.println("Of those, unique count: " + uniqueErr.size());
+		//Iterate over unique errors
 		Iterator it = uniqueErr.iterator();
 		System.out.println(" and they are: ");
 		while (it.hasNext()) {
 			String[] str = it.next().toString().split("\\.");
 			String out = str[1].substring(str[1].length() - 1) + "_" + str[2].substring(str[2].length() - 3);
-			//TO-DO split -> replace
-			//out = out.split("_").join(".");
-			System.out.println(str[0] + " - " + out);
+			System.out.println(str[0] + " - " + out.replace('_', '.'));
 		}
 		System.out.println();
+		
 		System.out.println("Warnings : " + j);
 		System.out.println("Of those, unique count: " + uniqueWarn.size());
+		//Iterate over unique warnings
 		it = uniqueWarn.iterator();
 		System.out.println(" and they are: ");
 		while (it.hasNext()) {
-			String[] str = it.next().toString().split("\\.");
-			String out = str[1].substring(str[1].length() - 1) + "_" + str[2].substring(str[2].length() - 3);
-			//TO-DO split -> replace
-			//out = out.split("_").join(".");
-			System.out.println(str[0] + " - " + out);
+			//Original output: WCAG2AA.Principle1.Guideline1_3.1_3_1.H48
+			String next = it.next().toString();
+			String[] output = parseOutput(next);
+			
 		}
 		
 		return false;
+	}
+	private String[] parseOutput(String next) {
+		
+		//To check whether output is right, uncomment next line
+		//System.out.print("Original: " + next + " --- ");
+		
+		Pattern p = Pattern.compile("WCAG2(\\S+)\\.Principle(\\d)\\.Guideline(\\d)_(\\d)\\..*");
+		Matcher m = p.matcher(next);
+		
+		if (m.find()) {
+			//A, AA or AAA
+			String WCAGlevel = m.group(1);
+			System.out.print(WCAGlevel + " - ");
+			
+			//Principle and guideline number, e.g "1.1.3"
+			String guideline = m.group(2) + "." + m.group(3) + "." + m.group(4);
+			System.out.println(guideline);
+		}
+		return ({WCAGlevel, guideline});
 	}
 }
