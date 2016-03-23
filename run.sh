06d83e35a850d23fa8639db52fe79c391e088671
Jaan Susi <jaan.susi@gmail.com>
2016-03-23 20:02:46 +0200

    Use key-pair instead of pass

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ cd /home/jaan/WCAGgrader/grader
 comm --nocheck-order -3 ../warcs ../parsed_warcs.txt > ../warcs_diff.txt
 for i in $(cat ../warcs_diff.txt); do
 	echo -n downloading $i...
-	sshpass -p '' scp jaan@deepweb.ut.ee:/mnt/$i .
+	scp jaan@deepweb.ut.ee:/mnt/$i .
 	echo downloaded
 	echo $i > cur_warc.txt
 	echo -n $i - > parsed_warcs_time.txt
